1ebadd05b08404695ab8ace841fb1d0609bc8334
Matt Swieboda <mswieboda@users.noreply.github.com>
2025-10-03 16:30:16 -0500

    tweaks jumps, max of 3, configurable

--- objs/player/player.gd
+++ objs/player/player.gd
@@ -6,17 +6,27 @@ extends CharacterBody3D
 @export var acceleration: float = 10.0
 @export var friction: float = 15.0
 
+# Jump variables
+@export var jumps_max: int = 3
+var jumps: int = 3
+
 # Get the gravity from the project settings
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta: float) -> void:
     # Add gravity
-    if not is_on_floor():
+    if is_on_floor():
+        # resets jumps
+        jumps = jumps_max
+    else:
+        prints(">>> NOT is_on_floor")
         velocity.y -= gravity * delta
 
     # Handle jump
-    if Input.is_action_just_pressed("jump") and is_on_floor():
+    # TODO: add a delay/queue up jumps so they can't rapidly press jump button and waste it
+    if Input.is_action_just_pressed("jump") and jumps > 0:
         velocity.y = jump_velocity
+        jumps -= 1
 
     # Get input direction
     var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
